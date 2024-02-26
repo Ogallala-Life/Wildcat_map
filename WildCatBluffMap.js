68b96d869c2f6a6d9e9b0397c59e49459a750dfc
kirkblood <50108976+kirkblood@users.noreply.github.com>
2024-02-26 17:16:51 -0600

    Setting Default Description

--- WildCatBluffMap.js
+++ WildCatBluffMap.js
@@ -924,12 +924,9 @@ structurePoints.forEach(point => {
   console.log(point.images);
 
   //Remove this line when descriptions are being set
-  point.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
-  console.log(point.description)
+  point.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
 
 });
-
-console.log(structurePoints[0])
 
 
 
