1ab19e596ea9bffd64c542d9b66742e11bdf60cb
Scott Kilgore <44659733+ScottTpirate@users.noreply.github.com>
2024-02-26 23:20:30 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', function() {
     }).addTo(map);
 
     var normalIcon = L.icon({
-        iconUrl: 'assets/leaf-green.png',
-        shadowUrl: 'assets/leaf-shadow.png',
+        iconUrl: './assets/leaf-green.png',
+        shadowUrl: './assets/leaf-shadow.png',
         iconSize:     [38, 95], // size of the icon
         shadowSize:   [50, 64], // size of the shadow
         iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
@@ -26,8 +26,8 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     
     var highlightedIcon = L.icon({
-        iconUrl: 'assets/leaf-red.png',
-        shadowUrl: 'assets/leaf-shadow.png',
+        iconUrl: './assets/leaf-red.png',
+        shadowUrl: './assets/leaf-shadow.png',
         iconSize:     [38, 95], // size of the icon
         shadowSize:   [50, 64], // size of the shadow
         iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
