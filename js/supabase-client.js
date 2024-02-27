2302d182ce64d436a69427da0a84293ea37ed1c7
Scott Kilgore <44659733+ScottTpirate@users.noreply.github.com>
2024-02-26 22:59:53 -0700

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.7/+esm'
 
-const supabaseUrl = 'https://evnikmzotvzhcykurhch.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2bmlrbXpvdHZ6aGN5a3VyaGNoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwOTAwMDk4MywiZXhwIjoyMDI0NTc2OTgzfQ.G1hqGsLzO8B1Uaq7h0p9njBssmrM8O_YFG7q-ezSDuo';
+const supabaseUrl = '${{ secrets.SUPABASE_URL }}';
+const supabaseKey = '${{ secrets.SUPABASE_KEY }}';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function fetchPoints(callback) {
